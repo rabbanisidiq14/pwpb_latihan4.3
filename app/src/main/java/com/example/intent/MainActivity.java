1b15d81ed3fbb7706cd6246ee8dba14dc74ed271
rabbanisidiq14 <rabbanisidiq@gmail.com>
2020-08-20 00:28:18 +0700

    Alpha

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText name = (EditText) findViewById(R.id.edit_textdata);
+        Button button = (Button) findViewById(R.id.button_submitintent);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("name",name.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/intent/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.intent;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = (TextView) findViewById(R.id.text_viewdata);
+        Bundle bundle = getIntent().getExtras();
+        String s = bundle.getString("name");
+        t.setText(s);
+    }
+}
